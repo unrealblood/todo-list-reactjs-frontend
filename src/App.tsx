b55d017e53a3a97256c8b2e2512e7994d9233a67
Shubham Singh <shubham110084@outlook.com>
2025-11-27 15:36:43 +0530

    create, link Footer component to App.tsx component and beautified ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { useState } from "react";
 import { AuthHeader } from "./components/header/AuthHeader";
 import { AllRoutes } from "./routes/AllRoutes";
 import { UnauthHeader } from "./components/header/UnauthHeader";
+import { Footer } from "./components/footer/Footer";
 
 function App() {
   const [isAuthUser, setIsAuthUser] = useState<boolean>(true);
 
   return (
-    <div className="p-2">
+    <div className="p-2 flex flex-col min-h-screen">
       {isAuthUser ? <AuthHeader /> : <UnauthHeader />}
-      <AllRoutes />
+
+      <div className="flex-1 overflow-auto">
+        <AllRoutes />
+      </div>
+      
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="p-2 border-t border-gray-200">
+            <div className="text-center">&copy; Copyright 2025 Shubham Singh. All Rights Reserved.</div>
+        </footer>
+    );
+}
+
+export {Footer};
